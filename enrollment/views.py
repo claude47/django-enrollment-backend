47d0aa2419c172c148c8d51bad8f328cdb523ab7
Claude47 <daruca.claudio@gmail.com>
2024-04-16 10:37:04 +0800

    traversed subjects and students enrolled

--- enrollment/views.py
+++ enrollment/views.py
@@ -14,3 +14,4 @@ class EnrollmentViewSet(viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
--- subjects/views.py
+++ subjects/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import Subject
 from .serializers import SubjectSerializer
+from enrollment.models import Enrollment
+from students.serializers import StudentSerializer
 
 class SubjectViewSet(viewsets.ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+    def retrieve(self, request, pk=None):
+        subject = get_object_or_404(Subject, pk=pk)
+        enrolled_students = Enrollment.objects.filter(subject=subject)
+        students = [enrollment.student for enrollment in enrolled_students]
+        serialized_students = StudentSerializer(students, many=True)
+        serialized_subject = self.get_serializer(subject)
+
+        data = {
+            'subject': serialized_subject.data,
+            'enrolled_students': serialized_students.data
+        }
+
+        return Response(data, status=status.HTTP_200_OK)
