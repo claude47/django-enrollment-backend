8b60460cc14dbac98f64f0700518bf873ac6fd98
claude47 <daruca.claudio@gmail.com>
2024-04-24 09:27:19 +0800

    reverted pagination

--- teachers/views.py
+++ teachers/views.py
@@ -7,9 +7,6 @@ from .models import Teacher
 from .serializers import TeacherSerializer
 from enrollment.models import Enrollment
 from students.serializers import StudentSerializer
-from rest_framework.pagination import PageNumberPagination
-
-from rest_framework.pagination import PageNumberPagination
 
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
@@ -20,11 +17,7 @@ class TeacherViewSet(viewsets.ModelViewSet):
         students_under = Enrollment.objects.filter(teacher=teacher)
         students_count = students_under.aggregate(count=Count('student'))['count']
         students = [enrollment.student for enrollment in students_under]
-
-        # Manually apply pagination to the students list
-        paginator = PageNumberPagination()
-        paginated_students = paginator.paginate_queryset(students, request)
-        serialized_students = StudentSerializer(paginated_students, many=True)
+        serialized_students = StudentSerializer(students, many=True)
 
         serialized_teacher = self.get_serializer(teacher)
 
@@ -34,4 +27,4 @@ class TeacherViewSet(viewsets.ModelViewSet):
             'enrolled_students': serialized_students.data  
         }
 
-        return paginator.get_paginated_response(data)
+        return Response(data, status=status.HTTP_200_OK)
